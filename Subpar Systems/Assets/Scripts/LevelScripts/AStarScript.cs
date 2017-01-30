127fd63636d8c52a16d765046df5a89a6207cec2
KynanTLy <kynan.ly@hotmail.ca>
2017-01-29 18:37:05 -0700

    Got most of the helpers ready for AStar

--- Subpar Systems/Assets/Scripts/LevelScripts/AStarScript.cs
+++ Subpar Systems/Assets/Scripts/LevelScripts/AStarScript.cs
@@ -29,11 +29,76 @@ public class AStarScript : MonoBehaviour {
 	public List<GameObject> findShitestPath(List<List<GameObject>> map, int originRow, int originIndex, int goalRow, int goalIndex)
 	{
 		Debug.Log ("Testing Return Direction");
-		Debug.Log(returnDirection (originIndex, originIndex, true, true));
+		Debug.Log (originRow + " " + originIndex);
+		//Debug.Log (originIndex);
+		Debug.Log (goalRow + " " +  goalIndex);
+		//Debug.Log (goalIndex);
+		Debug.Log ("The end of the Stats");
+		Debug.Log(ReturnDirection (originIndex, goalIndex, originRow, goalRow));
 
 		return null;
 	}
 
+	//Calculate an arbitary HeuristicCost that is basically Eucliden distance
+	private int CalculateHeuristicCost(int originRow, int originIndex, int goalRow, int goalIndex)
+	{
+		//There are temp values for cost matrix
+		int diagonalCost = 4;
+		int cardinalCost = 2;
+
+		//Calculate an arbitrary cost value
+		float min = Mathf.Min (Mathf.Abs (originRow - goalRow), Mathf.Abs (originIndex - goalIndex));
+		float max = Mathf.Max (Mathf.Abs (originRow - goalRow), Mathf.Abs (originIndex - goalIndex));
+
+		//Current return cost is arbitary
+		return ((min*diagonalCost) + ((max-min)*cardinalCost));
+	}
+
+	//Check can I move from this tile to the next
+	private bool CanGetNext(int originRow, int originIndex, int goalRow, int goalIndex)
+	{
+		//Check generic bounds
+		int maxRow = 0;
+		int maxIndex = 0;
+		if (!CheckBound(goalRow,goalIndex, maxRow, maxIndex) 
+			|| !CheckOneTileAway(originRow,originIndex,goalRow,goalIndex) 
+			|| CheckIsSelf(originRow,originIndex,goalRow,goalIndex)) {
+			return false;
+		}
+
+		//Code to check if it is matching earthtile
+		//Code might go here for "terrian cost stuff" 
+		return true;
+	}
+
+	//Check to see the tile is itself
+	private bool CheckIsSelf(int originRow, int originIndex, int goalRow, int goalIndex){
+		if (originRow == goalRow && originIndex == goalIndex) {
+			return true;
+		}
+		return false;
+	}//end CheckIsSelf
+
+	//Check to see if the tile is 1 tile away
+	private bool CheckOneTileAway(int originRow, int originIndex, int goalRow, int goalIndex)
+	{
+		if (Mathf.Abs (originRow - goalRow) > 1 || Mathf.Abs (originIndex - goalIndex) > 1) {
+			return false;
+		}
+		return true;
+	}//end checkOneTileAway
+
+	//Check to see if the next tile is within the gamebound
+	private bool CheckBound(int goalRow, int goalIndex, int maxRow, int maxIndex)
+	{
+		if (goalRow < 0 || goalRow > maxRow) {
+			return false;
+		}
+		if (goalIndex < 0 || goalIndex > maxIndex) {
+			return false;
+		}
+		return true;
+	}//end checkBound
 
 	/*
 	 *	NE = 0 
@@ -41,13 +106,15 @@ public class AStarScript : MonoBehaviour {
 	 *	SE = 2
 	 *	SW = 3
 	*/
-	public int returnDirection(int originIndex, int neighbourIndex, bool isAbove, bool isOddRow)
+	//Return the direction the unit is takes when moving from current location to next tile
+	private int ReturnDirection(int originIndex, int neighbourIndex, int originRow, int neighbourRow)
 	{
-		if (!isOddRow) {
+		
+		if (originRow%2 == 0) {
 			//Check Even row
-			if (!isAbove) {
+			if (neighbourRow > originRow) {
 				if (originIndex == neighbourIndex) {
-					return 0; //NE	
+					return 3; //SW	
 				} else {
 					return 2; //SE
 				}
@@ -55,20 +122,20 @@ public class AStarScript : MonoBehaviour {
 				if (originIndex == neighbourIndex) {
 					return 1; //NW	
 				} else {
-					return 3; //SW
+					return 0; //NE
 				}			
 			}
 		} else {
 			//Check Odd rows
-			if (!isAbove) {
+			if (neighbourRow > originRow) {
 				if (originIndex == neighbourIndex) {
 					return 2; //SE	
 				} else {
-					return 0; //NE
+					return 3; //SW
 				}
 			} else {
 				if (originIndex == neighbourIndex) {
-					return 3; //SW	
+					return 0; //NE
 				} else {
 					return 1; //NW
 				}			
@@ -77,6 +144,6 @@ public class AStarScript : MonoBehaviour {
 
 		//If it get here it is broken
 		return -1;
-	}
+	}//end returnDirection
 
 }
