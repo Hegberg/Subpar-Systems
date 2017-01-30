43a8067e6077e11088a206607afa838be04c0311
KynanTLy <kynan.ly@hotmail.ca>
2017-01-30 12:45:43 -0700

    Finished AStar, NOT TESTED

--- Subpar Systems/Assets/Scripts/LevelScripts/AStarScript.cs
+++ Subpar Systems/Assets/Scripts/LevelScripts/AStarScript.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class AStarScript : MonoBehaviour {
 	public static AStarScript control;
+	private int maxRow;
+	private int maxIndex;
 	// Use this for initialization
 	void Start () {
 		if(control == null)
@@ -26,7 +28,10 @@ public class AStarScript : MonoBehaviour {
 		LevelControlScript.control.GetAStarMap();
 	}
 
-	public List<GameObject> findShitestPath(List<List<GameObject>> map, int originRow, int originIndex, int goalRow, int goalIndex)
+	/*
+		WARNING NONE OF THE FOLLOWING CODE IS TEST AND WILL AND COULD BREAK EVERYTHING THAT EXIST!
+	*/
+	public List<List<int>> findShitestPath(List<List<GameObject>> map, int originRow, int originIndex, int goalRow, int goalIndex)
 	{
 		Debug.Log ("Testing Return Direction");
 		Debug.Log (originRow + " " + originIndex);
@@ -34,10 +39,158 @@ public class AStarScript : MonoBehaviour {
 		Debug.Log (goalRow + " " +  goalIndex);
 		//Debug.Log (goalIndex);
 		Debug.Log ("The end of the Stats");
-		Debug.Log(ReturnDirection (originIndex, goalIndex, originRow, goalRow));
+		Debug.Log(ReturnDirection (originRow, originIndex, goalRow, goalIndex));
 
-		return null;
+
+		//Initialize variables for gScore, fScore, starting position, goal position
+		//For dictionary the List => the row/index number of the tile
+		var gScore = new Dictionary<List<int>, int>();
+		var fScore = new Dictionary<List<int>, int>();
+
+		maxRow = map.Count;
+		maxIndex = map [0].Count;
+
+		List<List<int>> openSet = new List<List<int>> ();
+		Dictionary<List<int>, List<int>> cameFromSet =  new Dictionary<List<int>, int>();
+
+		//Init Starting and goal position
+		List<int> startPosition = new List<int> ();
+		startPosition.Add (originRow);
+		startPosition.Add (originIndex);
+		List<int> goalPosition = new List<int> ();
+		goalPosition.Add (goalRow);
+		goalPosition.Add (goalIndex);
+
+		//INSERT SOMETHING ABOUT GENERATION COUNTER
+
+		//Init all values of gScore/fScore to infinity
+		// i = Row
+		// j = Index
+		List<int> tempTilePosition = new List<int> ();
+		for (int i = 0; i < map.Count; ++i)
+		{
+			for (int j = 0; i < map[i].Count; ++j) 
+			{
+				//Clear the old values
+				tempTilePosition.Clear ();
+				//Add the new row and index
+				tempTilePosition.Add (i);
+				tempTilePosition.Add (j);
+
+				gScore [tempTilePosition] = int.MaxValue;
+				fScore [tempTilePosition] = int.MaxValue;
+			}//end inner for loop
+		}//end for loop
+
+		//Init start node
+		gScore[startPosition] = 0;
+		fScore [startPosition] = CalculateHeuristicCost (originRow, originIndex, goalRow, goalIndex);
+
+		//Insert the starting node 
+		openSet.Add(startPosition);
+
+		//Visit the nodes in openSet
+		List<int> currentNode = new List<int> ();
+		List<int> neighborNode = new List<int> ();
+		int lowestFScore;
+		while(!openSet.Count == 0)
+		{
+			//Clear the current node
+			currentNode.Clear();
+
+			//Find the lowest fScore in the opensEt
+			lowestFScore = int.MaxValue;
+			for (int i = 0; i < openSet.Count; ++i) 
+			{
+				if(fScore[openSet[i]] < lowestFScore)
+				{
+					lowestFScore = fScore[openSet[i]];
+					currentNode = openSet [i];
+				}
+			}//end for loop
+
+			//Found the goal
+			if (currentNode.Equals (goalPosition)) 
+			{
+				return ReconstructPath (cameFromSet, currentNode);
+			}
+
+			//Pop the currentNode from openSet
+			openSet.Remove(currentNode);
+			//INSERT SOMETHING ABOUT GENERATION COUNTER
+
+			int currentNodeRow = currentNode [0];
+			int currentNodeIndex = currentNode [1];
+
+			//Iterate through all the surrounding nodes
+			for (int gRow = currentNodeRow - 1; i < currentNodeRow + 2; ++gRow) 
+			{
+				for (int gIndex = currentNodeIndex - 1; j < currentNodeIndex + 2; ++gIndex) 
+				{
+					if (CanGetNext (currentNodeRow, currentNodeIndex, gRow, gIndex)) 
+					{
+						//Add the new neighbor
+						neighborNode.Clear ();
+						neighborNode.Add (gRow);
+						neighborNode.Add (gIndex);
+
+						//INSERT SOMETHING ABOUT GENERATION COUNTER
+
+						int tentativeGScore = gScore [currentNode] + ReturnCostTile (ReturnDirection, currentNodeRow, currentNodeIndex, goalRow, gIndex);
+
+						//Add the neighbor to the openset if it not there
+						//Also check to see if the tentativeGScore is less than the one current stored for neighbor
+						if (!openSet.Find (neighborNode)) {
+							openSet.Add (neighborNode);
+						} 
+						else if (tentativeGScore >= gScore [neighborNode]) 
+						{
+							continue;
+						}
+						//Create a dic that contains the  Row/Index of current (where we came from)  VALUE
+						//								  Row/Index of neighbor (Where we are going) KEY
+
+						cameFromSet[neighborNode] = currentNode;
+
+						//Change the related gScore and fScore
+						gScore [neighborNode] = tentativeGScore;
+						fScore [neighborNode] = gScore [neighborNode] + CalculateHeuristicCost (gRow, gIndex, goalRow, goalIndex);
+
+
+					}
+				}//end for loop
+			
+			}//end for loop
+
+		}//end while loop
+
+		//We should never get HERE. LIKE EVER
+		return 0;
 	}
+
+	//Parse through the cameFromList and returns a single path
+	//We start from the end goal and go backwards
+	//Remeber that the dictionary has the "destination" as key and "came from" as value
+	private List<List<int>> ReconstructPath(Dictionary <List<int>, List<int>> cameFromDic, List<int> currentNode)
+	{
+		List<int> current = currentNode;
+		List<List<int>> finalPath = new List<List<int>> ();
+
+		for (int i = 0; i < cameFromDic.Count; ++i) 
+		{
+			if (cameFromDic.ContainsKey (current)) 
+			{
+				//Add the current to final path
+				finalPath.Add (current);
+				//Set the next current as the "came from"
+				current = cameFromDic [current];
+			}
+		}//end for loop
+
+		//Return back the proper order
+		return finalPath.Reverse ();
+
+	}//end ReconstructPath
 
 	//Calculate an arbitary HeuristicCost that is basically Eucliden distance
 	private int CalculateHeuristicCost(int originRow, int originIndex, int goalRow, int goalIndex)
@@ -57,10 +210,8 @@ public class AStarScript : MonoBehaviour {
 	//Check can I move from this tile to the next
 	private bool CanGetNext(int originRow, int originIndex, int goalRow, int goalIndex)
 	{
-		//Check generic bounds
-		int maxRow = 0;
-		int maxIndex = 0;
-		if (!CheckBound(goalRow,goalIndex, maxRow, maxIndex) 
+
+		if (!CheckBound(goalRow,goalIndex) 
 			|| !CheckOneTileAway(originRow,originIndex,goalRow,goalIndex) 
 			|| CheckIsSelf(originRow,originIndex,goalRow,goalIndex)) {
 			return false;
@@ -89,7 +240,7 @@ public class AStarScript : MonoBehaviour {
 	}//end checkOneTileAway
 
 	//Check to see if the next tile is within the gamebound
-	private bool CheckBound(int goalRow, int goalIndex, int maxRow, int maxIndex)
+	private bool CheckBound(int goalRow, int goalIndex)
 	{
 		if (goalRow < 0 || goalRow > maxRow) {
 			return false;
@@ -100,6 +251,13 @@ public class AStarScript : MonoBehaviour {
 		return true;
 	}//end checkBound
 
+	//Return the cost of traveling 
+	private int ReturnCostTile(int tileCost)
+	{
+		//INSERT CODE FOR REAL COST
+		return tileCost;
+	}
+
 	/*
 	 *	NE = 0 
 	 * 	NW = 1
@@ -107,7 +265,7 @@ public class AStarScript : MonoBehaviour {
 	 *	SW = 3
 	*/
 	//Return the direction the unit is takes when moving from current location to next tile
-	private int ReturnDirection(int originIndex, int neighbourIndex, int originRow, int neighbourRow)
+	private int ReturnDirection( int originRow, int originIndex, int neighbourRow, int neighbourIndex)
 	{
 		
 		if (originRow%2 == 0) {
