8c1563ff0d5d2e459c4b96f1e3ae4a638c05a523
Chance Pearson <capearso@ualberta.ca>
2017-02-12 21:15:36 -0700

    Committing UI
    UI changes. Dont push to Master yet, as now working on getting characters to push into game.

---
+++ TitleText.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleText : MonoBehaviour {
+    Text mission;
+	// Use this for initialization
+	void Start () {
+        mission = GetComponent<Text>();
+        mission.text = "Mission One";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
