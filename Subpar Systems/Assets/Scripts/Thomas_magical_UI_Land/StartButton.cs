64e7b6d3ece1b0fe4cd4a090653feb0446c0bd48
Thomas Curnow <curnow@ualberta.ca>
2017-03-18 13:25:54 -0600

    Main Main UI 2.0

---
+++ Subpar Systems/Assets/Scripts/Thomas_magical_UI_Land/StartButton.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+
+	}
+
+    void OnMouseEnter()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(true);
+        }
+    }
+
+
+    void OnMouseExit()
+    {
+        foreach (Transform child in transform)
+        {
+            child.gameObject.SetActive(false);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("HubArea");
+    }
+}
