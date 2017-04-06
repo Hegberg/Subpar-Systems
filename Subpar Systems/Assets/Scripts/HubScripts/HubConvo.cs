0e71a6aff308e9b3e022c00c8c06790dfa0f3c00
Chance Pearson <capearso@ualberta.ca>
2017-04-06 02:15:00 -0600

    Hub Dialouge
    Added a text parser to hub dialouge. Should allow us to add scences
    quickly

--- Subpar Systems/Assets/Scripts/HubScripts/HubConvo.cs
+++ Subpar Systems/Assets/Scripts/HubScripts/HubConvo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,51 +8,58 @@ public class HubConvo : MonoBehaviour {
     Button go;
     string sentence;
     private List<string> convo = new List<string>();
+    private List<int> speaker = new List<int>();
+    protected FileInfo theSourceFile = null;
+    protected StreamReader reader = null;
+    protected string text = " ";
+
     // Use this for initialization
-    void Start () {
-        go = GetComponent<Button>();
-        go.onClick.AddListener(clicked);
-        convo.Add("test");
-        convo.Add("test2");
-        convo.Add("test3");
-        convo.Add("test4");
-        convo.Add("test5");
-    }
-    public void clicked() {
-        StartCoroutine(OnMouseOver()); 
-}
-    // Update is called once per frame
-    IEnumerator OnMouseOver()
-    {
+    void Start() {
         var UI1 = GameObject.FindWithTag("MissionHubConvo2");
         var UI2 = GameObject.FindWithTag("MissionHubConvo1");
         var convo2 = GameObject.FindWithTag("Convo1");
         var convo1 = GameObject.FindWithTag("Convo2");
-        while (convo.Count != 0){
-            if (convo.Count != 0)
-            {
-                UI1.GetComponent<Image>().enabled = true;
-                convo1.GetComponent<Text>().enabled = true;
-                sentence = convo[0];
-                convo.RemoveAt(0);
-                convo1.GetComponent<Text>().text = sentence;
-                yield return new WaitForSecondsRealtime(3);//could be long?
-                UI1.GetComponent<Image>().enabled = false;
-                convo1.GetComponent<Text>().enabled = false;
-            }
-            if (convo.Count != 0)
-            {
-                sentence = convo[0];
-                convo.RemoveAt(0);
-                convo2.GetComponent<Text>().text = sentence;
-                UI2.GetComponent<Image>().enabled = true;
-                convo2.GetComponent<Text>().enabled = true;
-                yield return new WaitForSecondsRealtime(3);//could be long?
-                UI2.GetComponent<Image>().enabled = false;
-                convo2.GetComponent<Text>().enabled = false;
-            }
-        }
-        //need to add trait adding 
+        theSourceFile = new FileInfo("HubScene1level1.txt");
+        reader = theSourceFile.OpenText();
+        go = GetComponent<Button>();
+        UI1.GetComponent<Image>().enabled = false;
+        convo1.GetComponent<Text>().enabled = false;
+        UI2.GetComponent<Image>().enabled = false;
+        convo2.GetComponent<Text>().enabled = false;
+        go.onClick.AddListener(clicked);
     }
-
+    private void clicked() {
+        var UI1 = GameObject.FindWithTag("MissionHubConvo2");
+        var UI2 = GameObject.FindWithTag("MissionHubConvo1");
+        var convo2 = GameObject.FindWithTag("Convo1");
+        var convo1 = GameObject.FindWithTag("Convo2");
+        if (text != null)
+        {
+            text = reader.ReadLine();
+            UI1.GetComponent<Image>().enabled = false;
+            convo1.GetComponent<Text>().enabled = false;
+            UI2.GetComponent<Image>().enabled = false;
+            convo2.GetComponent<Text>().enabled = false;
+        }
+        if (text == null) {
+            //error handle
+        }
+        else if (text.Substring(0, 1) == ("G"))
+        {
+            UI1.GetComponent<Image>().enabled = true;
+            convo1.GetComponent<Text>().enabled = true;
+            sentence = text.Substring(2);
+            convo1.GetComponent<Text>().text = sentence;
+        }
+        else if (text.Substring(0, 1) == ("T"))
+        {
+            UI2.GetComponent<Image>().enabled = true;
+            convo2.GetComponent<Text>().enabled = true;
+            sentence = text.Substring(2);
+            convo2.GetComponent<Text>().text = sentence;
+        }
+    }
 }
+    // Update is called once per frame
+
+
