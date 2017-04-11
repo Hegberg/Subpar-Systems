ed3310b85c2e75b748e1d715f8ea226d55c64470
Chance Pearson <capearso@ualberta.ca>
2017-04-11 16:49:47 -0600

    Deaths
    Hub Dialouges handle deaths now..... Still need to change alpha of icons
    to represent deaths....

--- Subpar Systems/Assets/HubController.cs
+++ Subpar Systems/Assets/HubController.cs
@@ -5,29 +5,46 @@ using UnityEngine.UI;
 
 public class HubController : MonoBehaviour {
     int missonlevel = 2;
-	// Use this for initialization
-	void Start () {
+    private List<string> deadpeoples = new List<string>();
+    // Use this for initialization
+    void Start()
+    {
         missonlevel = GameControlScript.control.GetLevel();
-        //Debug.Log(missonlevel);
-        if (missonlevel == 1)
+        deadpeoples = GameControlScript.control.GetDeadCharacters();
         {
-            var scene = GameObject.FindWithTag("Level 1");
-            scene.GetComponent<Canvas>().enabled = true;
+            //Debug.Log(missonlevel);
+            if (missonlevel == 1)
+            {
+                var scene = GameObject.FindWithTag("Level 1");
+                if (deadpeoples.Contains("Geoff")|| deadpeoples.Contains("Taliyah"))
+                {
+                    GameObject.FindWithTag("Level1A").GetComponent<Button>().enabled=false;
+                    //GameObject.FindWithTag("Level1A").GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.5f);
+                }
+                if (deadpeoples.Contains("Jai Ono") || deadpeoples.Contains("Ashe"))
+                {
+                    GameObject.FindWithTag("Level1B").GetComponent<Button>().enabled = false;
+                    //GameObject.FindWithTag("Level1B").GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.5f);
+                }
+                scene.GetComponent<Canvas>().enabled = true;
+            }
+            else if (missonlevel == 2)
+            {
+                var scene = GameObject.FindWithTag("Level 2");
+                scene.GetComponent<Canvas>().enabled = true;
+                if (deadpeoples.Contains("Lt-Col George Murphy") || deadpeoples.Contains("Roy LeGaul"))
+                {
+                    GameObject.FindWithTag("Level2A").GetComponent<Button>().enabled = false;
+                    //GameObject.FindWithTag("Level2A").GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 0.5f);
+                }
+                //activate scenes for 2
+            }
+            else
+            {
+                //level 3
+            }
         }
-        else if (missonlevel == 2)
-        {
-            var scene = GameObject.FindWithTag("Level 2");
-            scene.GetComponent<Canvas>().enabled = true;
-            //activate scenes for 2
-        }
-        else
-        {
-            //level 3
-        }
-    }
 
-    // Update is called once per frame
-    void Update () {
-		
-	}
+        // Update is called once per frame
+    }
 }
