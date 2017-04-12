ea50c1a1850e337d70df7750a528ad2b10ec7666
Chance Pearson <capearso@ualberta.ca>
2017-04-11 23:47:56 -0600

    Traits
    Added the traits to the menu items

--- Subpar Systems/Assets/Traits.cs
+++ Subpar Systems/Assets/Traits.cs
@@ -91,10 +91,6 @@ public class Traits : MonoBehaviour {
                 {
                     text += "Wimp: Almost as bad as the weenie." + "\r\n" + "\r\n";
                 }
-                else if (trait == "WimpTrait")
-                {
-                    text += "Wimp: Almost as bad as the weenie." + "\r\n" + "\r\n";
-                }
                 else if (trait == "MalnourishedTrait")
                 {
                     text += "Malnourished: You are hungry...You should eat something." + "\r\n" + "\r\n";
@@ -138,6 +134,78 @@ public class Traits : MonoBehaviour {
                 else if (trait == "TankTrait")
                 {
                     text += "Tank: What more do you need to know. It is a tank." + "\r\n" + "\r\n";
+                }
+                else if (trait == "SchoolBonds")
+                {
+                    text += "School Bonds: Those that say the power of friendship is fake, has never had a true friend." + "\r\n" + "\r\n";
+                }
+                else if (trait == "PersonalGrudges")
+                {
+                    text += "Grudge: Ask one of them for their side of the story sometime." + "\r\n" + "\r\n";
+                }
+                else if (trait == "LackOfHumour")
+                {
+                    text += "Lack of Humour: The Canuckistans are… unfamiliar with troops expressing themselves, and strongly dislike unprofessionalism. Roy’s mercenaries demonstrate both in abundance." + "\r\n" + "\r\n";
+                }
+                else if (trait == "DistractingThoughts")
+                {
+                    text += "Distracting Thoughts: While some find love to be a motivating factor, others can find the thoughts of an unrequited love… distracting." + "\r\n" + "\r\n";
+                }
+                else if (trait == "CanuckistanEquipment")
+                {
+                    text += "Canuckistan Equipment: The Canuckistan trademark slow and steady approach with overwhelming firepower extends even to their assault troops. " + "\r\n" + "\r\n";
+                }
+                else if (trait == "Patriotism")
+                {
+                    text += "Patriotism: This unit shows a strong love towards the Northlands, and will perform better when Northland lives are endangered. " + "\r\n" + "\r\n";
+                }
+                else if (trait == "SSTraining")
+                {
+                    text += "SS Training: The SS trains all their troops for a large variety of situations. While they might not act like it, each member of the SS is an elite soldier more experienced than even the most worldly peacekeeper." + "\r\n" + "\r\n";
+                }
+                else if (trait == "Attachment")
+                {
+                    text += "Attachment: This unit feels a strong attachment to another officer." + "\r\n" + "\r\n";
+                }
+                else if (trait == "Hardworking")
+                {
+                    text += "Hardworking: Whether it’s farm work, taking care of family, studying at a university, or even military combat, Larry works hard and stays focused. His determination extends to his squad, who seem to be able to take a little bit more punishment than the other squads." + "\r\n" + "\r\n";
+                }
+                else if (trait == "BerserkRage")
+                {
+                    text += "Berserk Rage: Some might think that being angry all the time will leave Jai exhausted. Those thoughts only make Jai more angry." + "\r\n" + "\r\n";
+                }
+                else if (trait == "AngerIssues")
+                {
+                    text += "Anger Issues: While you can’t blame the monsters for trying, soon they realize that any wound Jai receives only fuels her hatred for them. " + "\r\n" + "\r\n";
+                }
+                else if (trait == "TooAngryToFeelPain")
+                {
+                    text += "Too Angry To Feel Pain: Wow, look at Ono. She’s covered in monster blood. Wait, no, that’s her blood. How the hell is she still standing?" + "\r\n" + "\r\n";
+                }
+                else if (trait == "HandsOffLeaderShip")
+                {
+                    text += "Hands Off Leadership: Devi has no interest in firing a weapon herself. One less machine gunner in a squad can have a surprising effect on damage output." + "\r\n" + "\r\n";
+                }
+                else if (trait == "Comradery")
+                {
+                    text += "Comradery: Has developed a strong working relationship with others." + "\r\n" + "\r\n";
+                }
+                else if (trait == "RunningTally")
+                {
+                    text += "Running Tally: sees each confirmed kill under her belt as a chance for advancement, and so she keeps a tally of the number of enemies that her unit personally slays. For this reason, she gets very excited when she gets to kill mobs of enemies, as it makes her both look good and feel good." + "\r\n" + "\r\n";
+                }
+                else if (trait == "LovedByTroops")
+                {
+                    text += "Loved By Troops: Any officer can make a soldier follow orders. It takes an exceptional one to make soldiers want to follow orders. " + "\r\n" + "\r\n";
+                }
+                else if (trait == "DrugAddiction")
+                {
+                    text += "Drug Addiction: The seemingly randomness of the portals means that there’s always a chance for supplies to fail to arrive on time, even illegal ones. When he does well, his squad does well… but the inverse is also true." + "\r\n" + "\r\n";
+                }
+                else if (trait == "SomethingToLiveFor")
+                {
+                    text += "Something To Live For: has a greater purpose in life than the military that he feels he needs to live for" + "\r\n" + "\r\n";
                 }
                 else
                 {
